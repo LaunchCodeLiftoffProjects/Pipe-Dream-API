325dbfb497aa39edc4c35cc92bb79891dab1abe4
lindskelsey <lindsaykelsey@gmail.com>
2020-03-09 18:37:53 -0500

    initial commit :tada:

---
+++ src/main/java/org/launchcode/pipedream/PipeDreamApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.pipedream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeDreamApplication.class, args);
+	}
+
+}
