9f16cd6af6b7a7217325f52c35f05b5eee1a93a8
lindskelsey <lindsaykelsey@gmail.com>
2020-03-09 18:24:49 -0500

    initial commit :tada:

---
+++ src/main/java/org/launchcode/pipedreamers/PipeDreamersApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.pipedreamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeDreamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeDreamersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/pipedreamers/PipeDreamersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.pipedreamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeDreamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
