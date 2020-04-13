d906f42921bda08a31d9ead71e8e3393c55b3b2f
name <email@email.com>
2020-04-12 20:49:35 -0500

    made changes to Controller and Restroom

--- src/main/java/org/launchcode/pipedream/controller/RestroomController.java
+++ src/main/java/org/launchcode/pipedream/controller/RestroomController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins={"http://localhost:3000"})
 @RestController
-@RequestMapping("/restroom")
+@RequestMapping("/restrooms")
 public class RestroomController {
 
     private RestroomService restroomService;
